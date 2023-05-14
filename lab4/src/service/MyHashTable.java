3db8470d2756cf2e6e3dcc64a9536f742d542263
shadyman <m.m.shdmn@gmail.com>
2023-05-14 11:58:23 +0600

    MyHashTable

--- lab4/src/service/MyHashTable.java
+++ lab4/src/service/MyHashTable.java
@@ -18,7 +18,7 @@ public class MyHashTable<K, V> {
     private int M = 11;
     private int size;
     public MyHashTable(){
-
+        this.chainArray = new HashNode[M];
     }
     public MyHashTable(int M){
 
